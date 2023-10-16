2e267a7735a9e03d846113a3c6765e8c5912f7d6
Elena Kechko <ekechko@gmail.com>
2023-10-16 20:57:29 +0300

    Update build.gradle file Finish working with main jar file

--- src/main/java/com/task/StringUtils.java
+++ src/main/java/com/task/StringUtils.java
@@ -1,6 +1,14 @@
 package com.task;
 
+import java.util.Arrays;
+
 public class StringUtils {
+    public static void main(String[] args) {
+        StringUtils stringUtils = new StringUtils();
+        Arrays.stream(args)
+                .forEach(arg ->
+                        System.out.printf("%s - %s\n", arg, stringUtils.isPositiveNumber(arg)));
+    }
 
     boolean isPositiveNumber(String str) {
         return Integer.parseInt(str) > 0;
