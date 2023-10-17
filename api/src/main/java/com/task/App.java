0fb18e42fc65daee25d5b0c4c2e5e87931c52733
Elena Kechko <ekechko@gmail.com>
2023-10-17 16:38:58 +0300

    Tests for isAllPositiveNumbers

--- api/src/main/java/com/task/App.java
+++ api/src/main/java/com/task/App.java
@@ -2,8 +2,8 @@ package com.task;
 
 public class App {
     public static void main(String[] args) {
-
-        System.out.println(Utils.isAllPositiveNumbers("12", "79"));
-        System.out.println(Utils.isAllPositiveNumbers("-12", "79"));
+//        System.out.println(Utils.isAllPositiveNumbers("12", "79"));
+//        System.out.println(Utils.isAllPositiveNumbers("-12", "79"));
+        System.out.println(Utils.isAllPositiveNumbers(args));
     }
 }
---
+++ core/src/test/java/com/task/UtilsTest.java
@@ -0,0 +1,21 @@
+package com.task;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UtilsTest {
+
+    @Test
+    void isPositiveNumberTrue() {
+        assertTrue(Utils.isAllPositiveNumbers("10", "3"));
+        assertTrue(Utils.isAllPositiveNumbers("15"));
+        assertTrue(Utils.isAllPositiveNumbers("66", "1", "159"));
+    }
+    @Test
+    void isNotAllPositiveNumberTrue() {
+        assertFalse(Utils.isAllPositiveNumbers("17", "-2"));
+        assertFalse(Utils.isAllPositiveNumbers("-6", "78"));
+        assertFalse(Utils.isAllPositiveNumbers("-54", "-2", "-3"));
+    }
+}
