870034bf54b20a595dc069dc811d7a8cd8a6aae3
Elena Kechko <ekechko@gmail.com>
2023-10-17 18:56:29 +0300

    Fix test

--- core/src/test/java/com/util/UtilsTest.java
+++ core/src/test/java/com/util/UtilsTest.java
@@ -2,7 +2,9 @@ package com.util;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 
 class UtilsTest {
 
