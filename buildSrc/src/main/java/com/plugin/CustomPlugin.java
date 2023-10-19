cdb4d2d6a899d67a2a9059e83e979f372bc31de3
Elena Kechko <ekechko@gmail.com>
2023-10-19 16:13:05 +0300

    Realization plugin

--- buildSrc/src/main/java/com/plugin/CustomPlugin.java
+++ buildSrc/src/main/java/com/plugin/CustomPlugin.java
@@ -8,28 +8,14 @@ import org.gradle.api.tasks.Copy;
 public class CustomPlugin implements Plugin<Project> {
 
     @Override
-//    public void apply(Project project) {
-//        project.task("hello")
-//                .doLast(task -> System.out.println("Hello Gradle!"));
-//    }
-
     public void apply(Project project) {
-        project.getTasks().create("myTask", MyTask.class);
 
-        project.getTasks().register("copy_reports", Copy.class) {
-            duplicatesStrategy(DuplicatesStrategy.INCLUDE)
-
+        project.getTasks().create("copy_reports", Copy.class, task -> {
             for (Project myProject : project.getAllprojects()) {
-                System.out.println(myProject.projectDir.absolutePath + "\\build\\reports\\");
-                println myProject.rootDir.absolutePath + "\\jacocoReports\\" + myProject.name + "\\"
-
-                from myProject.projectDir.absolutePath + "\\build\\reports\\"
-                exclude "tests"
-                into myProject.rootDir.absolutePath + "\\jacocoReports\\${myProject.name}"
+                task.from(myProject.getProjectDir().getAbsolutePath() + "\\build\\reports\\jacoco\\");
+                task.into(myProject.getRootDir().getAbsolutePath() + "\\jacocoReports\\");
             }
-
-        }
-
+            task.setDuplicatesStrategy(DuplicatesStrategy.INCLUDE);
+        });
     }
-
 }
